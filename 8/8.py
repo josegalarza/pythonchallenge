d239d5f5f88a56c62f947dbeba3d0f9645573913
Jose <jose@canva.com>
2020-05-29 16:33:18 +1000

    8

---
+++ 8/8.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# Python Challenge - Level 8
+# http://www.pythonchallenge.com/pc/def/integrity.html
+
+# Looking at the source code
+un = 'BZh91AY&SYA\xaf\x82\r\x00\x00\x01\x01\x80\x02\xc0\x02\x00 \x00!\x9ah3M\x07<]\xc9\x14\xe1BA\x06\xbe\x084'
+pw = 'BZh91AY&SY\x94$|\x0e\x00\x00\x00\x81\x00\x03$ \x00!\x9ah3M\x13<]\xc9\x14\xe1BBP\x91\xf08'
+# Looks like bz2
+
+import bz2
+
+un2 = b'BZh91AY&SYA\xaf\x82\r\x00\x00\x01\x01\x80\x02\xc0\x02\x00 \x00!\x9ah3M\x07<]\xc9\x14\xe1BA\x06\xbe\x084'
+username = bz2.decompress(un2)
+
+pw2 = b'BZh91AY&SY\x94$|\x0e\x00\x00\x00\x81\x00\x03$ \x00!\x9ah3M\x13<]\xc9\x14\xe1BBP\x91\xf08'
+password = bz2.decompress(pw2)
+
+print(username, password)
+# huge, file
+
+# Next Level: http://www.pythonchallenge.com/pc/return/good.html
